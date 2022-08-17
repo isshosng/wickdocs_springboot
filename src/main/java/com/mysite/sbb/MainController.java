185969e70610402b1968f6d1b0c425d56bffaed3
isshosng <purehug@hanmail.net>
2022-08-17 14:25:56 +0900

    컨트롤러2

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+// 해당 애너테이션을 적용하면 해당 클래스는 스프링부트의 컨트롤러가 됨
+public class MainController {
+
+    @RequestMapping("/sbb") // 요청된 URL과 매핑 담당
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+}
