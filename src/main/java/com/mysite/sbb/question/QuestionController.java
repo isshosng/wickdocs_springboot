59307297bca733b1fc4148519f4d6c91ce70d430
isshosng <purehug@hanmail.net>
2022-08-29 15:50:50 +0900

    데이터 조회하여 템플릿에 전달하기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,14 +1,27 @@
 package com.mysite.sbb.question;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+/*
+롬복이 제공하는 애너테이션으로 final이 붙은 속성을 포함하는 생성자를 자동으로 생성하는 역할을 함
+ */
 @Controller
 public class QuestionController {
+
+    private final QuestionRepository questionRepository;
     @RequestMapping("/question/list")
-    @ResponseBody
-    public String list(){
-        return "question list";
+
+    public String list(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList",questionList);
+        return "question_list";
     }
 }
